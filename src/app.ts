705b8e9940a0533dbe9629b628b9169b925d1826
longtk26 <longtk26@gmail.com>
2023-09-21 17:35:08 +0700

    init project

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
+import "dotenv/config";
+
+const app = express();
+
+// init middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init routes
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to ecommerce API!",
+  });
+});
+
+// handle error
+
+export default app;
